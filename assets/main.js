15a4cb0de2bb2ce785bc0d640b71bd78bce1a6f6
marcocentonze <marco.centonze27@gmail.com>
2023-08-24 15:34:14 +0200

    add milestone 1

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,27 @@
 -va applicato uno sconto del 40% per gli over 65. */
 
 
+// Chiedere all'utente il numero dei chilometri da percorrere
+const numeroKm = Number(prompt('Quanti chilometri vuoi percorrere?'));
+const etaPasseggero = Number(prompt('Quanti anni ha il passeggero?'));
+
+// Calcola il prezzo del biglietto in base ai chilometri (0.21 € al km)
+const prezzoAlKm = 0.21;
+let prezzoIntero = numeroKm * prezzoAlKm;
+let prezzoScontato = 0;
+
+// Applica gli sconti in base all'età
+if (etaPasseggero < 18) {
+    prezzoScontato = prezzoIntero * 0.2;
+} else if (etaPasseggero > 65) {
+    prezzoScontato = prezzoIntero * 0.4;
+}
+
+// Calcola il prezzo finale
+let prezzoFinale = prezzoIntero - prezzoScontato;
+
+// Stampa il prezzo finale in console 
+console.log('Il tuo prezzo è di ' + prezzoFinale.toFixed(2) + '€');
+
+// Scrivere l'output in pagina 
+//document.getElementById("costo_ticket").innerHTML = 'Il tuo prezzo è di ' + prezzoFinale.toFixed(2) + '€';
