826cbe75b7c01d892ab4848f88715af7febe2c0a
marcocentonze <marco.centonze27@gmail.com>
2023-08-24 19:16:45 +0200

    add form working correctly

--- assets/main.js
+++ assets/main.js
@@ -5,82 +5,38 @@
 -il prezzo del biglietto è definito in base ai km (0.21 € al km)
 -va applicato uno sconto del 20% per i minorenni
 -va applicato uno sconto del 40% per gli over 65. */
+// Calcola il prezzo del biglietto in base ai chilometri (0.21 € al km)
 
-//funzione addeventlistener su button calcola + output dell'utente in console
+
+//creo la funzione e inserisco tutto dentro(fuori non funzionerebbe)
 const generate = document.getElementById('calcola').addEventListener("click", function() {
- const nomePasseggero = document.getElementById("name").value;
- console.log(nomePasseggero);
- const kmPercorrere = document.getElementById('km').value;
- console.log(kmPercorrere);
- const etaPasseggero = document.getElementById('age').value;
- console.log(etaPasseggero);
+    const nomePasseggero = document.getElementById("name").value;
+    console.log(nomePasseggero);
+    const kmPercorrere = Number(document.getElementById('km').value);
+    console.log(kmPercorrere);
+    const etaPasseggero = Number(document.getElementById('age').value);
+    console.log(etaPasseggero);
 
- if (kmPercorrere == 0 || nomePasseggero == 0) {
-    result.innerHTML = "Uno o più elementi da lei inseriti non sono accettati.Riprova"
-} 
-
-else  
-
-})
-    
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-    // if
-    if ((numeroKm) && (etaPasseggero)) {
-        // Calcolare il prezzo del biglietto
+    if (kmPercorrere == 0 || nomePasseggero == "" ) {
+        alert = ("Uno o più elementi da lei inseriti non sono accettati.Riprova");
+    } else {
         const prezzoAlKm = 0.21;
-        let prezzoIntero = numeroKm * prezzoAlKm;
-        let prezzoScontato = 0;
+        let prezzoIntero = kmPercorrere * prezzoAlKm;
+        let prezzoScontato = prezzoIntero;
 
+        // Applico gli sconti in base all'età
         if (etaPasseggero < 18) {
-            prezzoScontato = prezzoIntero * 0.2;
+            prezzoScontato = prezzoIntero * 0.8; // Sconto del 20%
         } else if (etaPasseggero > 65) {
-            prezzoScontato = prezzoIntero * 0.4;
+            prezzoScontato = prezzoIntero * 0.6; // Sconto del 40%
         }
 
-        let prezzoFinale = prezzoIntero - prezzoScontato;
+        // Calcolo il prezzo finale
+        let prezzoFinale = prezzoScontato;
 
-        // Aggiorna il costo del biglietto nel biglietto stesso
-        
-        document.getElementById("costo_biglietto").textContent = prezzoFinale.toFixed(2) + '€';
-
-        // Aggiorna il nome passeggero
+        // Aggiorno i dettagli nel biglietto del passeggero
         document.getElementById("nome_passeggero").textContent = nomePasseggero;
-    } 
+        document.getElementById("costo_biglietto").textContent = prezzoFinale.toFixed(2) + " €";
+    }
 });
 
-element.addEventListener("click", function() {
- console.log("")
-}); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Chiedere all'utente il numero dei chilometri da percorrere
-//const numeroKm = Number(prompt('Quanti chilometri vuoi percorrere?'));
-//const etaPasseggero = Number(prompt('Quanti anni ha il passeggero?'));
-
-
-
-// Scrivere l'output in pagina 
-//document.getElementById("costo_ticket").innerHTML = 'Il tuo prezzo è di ' + prezzoFinale.toFixed(2) + '€';
